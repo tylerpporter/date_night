6a9e5b93ec6a47536253886d84f7442e4de87de7
Tyler Porter <tylerpporter@gmail.com>
2020-01-29 21:29:09 -0700

    Pass test_it_creates_a_tree_of_nodes

--- lib/bst.rb
+++ lib/bst.rb
@@ -1,3 +1,5 @@
+require_relative 'node.rb'
+
 class BinarySearchTree
   attr_reader :root
 
@@ -8,9 +10,10 @@ class BinarySearchTree
   def insert(score, title)
     if @root.nil?
       @root = Node.new(score, title, -1)
+      @root.depth 
     else
-      @root.node_insert(score, title)
+      @root.insert_node(score, title)
     end
   end
-  
+
 end
--- test/bst_test.rb
+++ test/bst_test.rb
@@ -16,6 +16,15 @@ class BinarySearchTreeTest < Minitest::Test
   def test_it_creates_a_root_node
     @tree.insert(89, "Seven")
     assert_instance_of Node, @tree.root
-  end 
+  end
 
+  def test_it_creates_a_tree_of_nodes
+    @tree.insert(61, "Bill & Ted's Excellent Adventure")
+    @tree.insert(16, "Johnny English")
+    @tree.insert(92, "Sharknado 3")
+    @tree.insert(50, "Hannibal Buress: Animal Furnace")
+
+    assert_equal ({"Johnny English" => 16}), @tree.root.left.movie
+    assert_equal ({"Sharknado 3" => 92}), @tree.root.right.movie
+  end
 end
