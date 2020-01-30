ac20ba493f658ab2339be0818937eeeb3da727dc
Tyler Porter <tylerpporter@gmail.com>
2020-01-29 21:11:39 -0700

    Pass test_it_creates_a_root_node

--- lib/bst.rb
+++ lib/bst.rb
@@ -4,5 +4,13 @@ class BinarySearchTree
   def initialize
     @root = nil
   end
+
+  def insert(score, title)
+    if @root.nil?
+      @root = Node.new(score, title, -1)
+    else
+      @root.node_insert(score, title)
+    end
+  end
   
 end
--- test/bst_test.rb
+++ test/bst_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/bst.rb'
+require './lib/node.rb'
 
 class BinarySearchTreeTest < Minitest::Test
 
@@ -11,5 +12,10 @@ class BinarySearchTreeTest < Minitest::Test
   def test_it_exists
     assert_instance_of BinarySearchTree, @tree
   end
+
+  def test_it_creates_a_root_node
+    @tree.insert(89, "Seven")
+    assert_instance_of Node, @tree.root
+  end 
 
 end
