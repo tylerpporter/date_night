c6f845322bd9b5c54aa6dc1d1d0765eb6f04cddb
Tyler Porter <tylerpporter@gmail.com>
2020-02-01 22:22:30 -0700

    Height method working

--- test/bst_test.rb
+++ test/bst_test.rb
@@ -110,4 +110,16 @@ class BinarySearchTreeTest < Minitest::Test
     assert_equal 2, @tree.leaves
   end
 
+  def test_height_of_tree
+    @tree.insert(98, "Animals United")
+    @tree.insert(58, "Armageddon")
+    @tree.insert(36, "Bill & Ted's Bogus Journey")
+    @tree.insert(93, "Bill & Ted's Excellent Adventure")
+    @tree.insert(86, "Charlie's Angels")
+    @tree.insert(38, "Charlie's Country")
+    @tree.insert(69, "Collateral Damage")
+
+    assert_equal 4, @tree.height
+  end
+
 end
